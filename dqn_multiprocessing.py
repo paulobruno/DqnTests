98ce99eb070a59ff83037501aeb2742650e49c8d
Paulo Bruno <pbrunosousa@gmail.com>
2022-03-29 00:40:52 -0300

    Update multiprocessing DQN

--- dqn_multiprocessing.py
+++ dqn_multiprocessing.py
@@ -6,11 +6,11 @@ from tqdm import trange
 import itertools as it
 import numpy as np
 import os # to check if a model folder exists
-import multiprocess
 
 from random import sample, randint, random
 from time import time, sleep
-from multiprocessing import Process
+from multiprocessing import Process, Manager, current_process, Lock
+from multiprocessing.managers import BaseManager
 
 import tensorflow as tf
 from tensorflow import keras
@@ -40,10 +40,18 @@ episodes_to_watch = 5
 
 # TODO: virar args
 num_network_updates = 10000
-num_epochs = 25
-num_episodes = 200
+num_epochs = 1
+num_episodes = 500
 config_file = 'scenarios/basic/basic/basic.cfg'
+num_processes = 1
 
+
+# limit gpu usage
+# by default, tensorflow allocates all available memory
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+    
 
 # Converts and down-samples the input image
 def preprocess(img):
@@ -67,22 +75,25 @@ class ReplayMemory:
         self.size = 0
         self.pos = 0
 
-    def add_transition(self, s1, action, s2, reward, isterminal):
+    def add_transition(self, s1, action, s2, isterminal, reward):
         self.s1[self.pos, :, :, 0] = s1
         self.a[self.pos] = action
         if not isterminal:
             self.s2[self.pos, :, :, 0] = s2
-        self.r[self.pos] = reward
         self.isterminal[self.pos] = isterminal
+        self.r[self.pos] = reward
 
         self.pos = (self.pos + 1) % self.capacity
         self.size = min(self.size + 1, self.capacity)
 
     def get_sample(self, sample_size):
         i = sample(range(0, self.size), sample_size)
-        return self.s1[i], self.a[i], self.s2[i], self.r[i], self.isterminal[i]
-        
+        return self.s1[i], self.a[i], self.s2[i], self.isterminal[i], self.r[i]
+    
+    def get_size(self):
+        return self.size
 
+        
 def create_network(available_actions_count):
 
     inputs = keras.Input(shape=(resolution[0], resolution[1], 1), name='frame')
@@ -94,46 +105,54 @@ def create_network(available_actions_count):
     q = layers.Dense(available_actions_count, kernel_initializer='glorot_normal', bias_initializer=tf.constant_initializer(0.1), activation=None)(x)
     
     model = keras.Model(inputs=inputs, outputs=q, name='vizdoom_agent_model')
+
     model.compile(optimizer='adam', loss='mse')
     
     return model
     
     
 def learn(model, state, target_q):
+    print('learn.')
     loss = model.train_on_batch(x=state, y=target_q)
+    print('.learn')
     return loss
 
 
 def get_q_values(model, state):
-    return model.predict(state)
+    print('get_q_values.')
+    q_values = model.predict(state)
+    print('.get_q_values')
+    return q_values
 
 
 def get_best_action(model, state):
+    print('get_best_action.')
+    print(state.shape)
     s = state.reshape([1, resolution[0], resolution[1], 1])
-    return tf.argmax(get_q_values(model, s)[0])
+    a = tf.argmax(get_q_values(model, s)[0])
+    print('.get_best_action')
+    return a
             
 
-def learn_from_memory(q_network, memory):
+def learn_from_memory(model):
     """ Learns from a single transition (making use of replay memory).
     s2 is ignored if s2_isterminal """
 
-    for _ in (num_network_updates):
-    
-        # Get a random minibatch from the replay memory and learns from it.
-        if memory.size > batch_size:
-            s1, a, s2, r, isterminal = memory.get_sample(batch_size)
+    # Get a random minibatch from the replay memory and learns from it.
+    if memory.size > batch_size:
+        s1, a, s2, isterminal, r = memory.get_sample(batch_size)
 
-            q2 = np.max(get_q_values(model, s2), axis=1)
-            target_q = get_q_values(model, s1)
-            # target differs from q only for the selected action. The following means:
-            # target_Q(s,a) = r + gamma * max Q(s2,_) if not isterminal else r
-            target_q[np.arange(target_q.shape[0]), a] = r + discount_factor * (1 - isterminal) * q2        
-            learn(model, s1, target_q)
+        q2 = np.max(get_q_values(model, s2), axis=1)
+        target_q = get_q_values(model, s1)
+        # target differs from q only for the selected action. The following means:
+        # target_Q(s,a) = r + gamma * max Q(s2,_) if not isterminal else r
+        target_q[np.arange(target_q.shape[0]), a] = r + discount_factor * (1 - isterminal) * q2        
+        learn(model, s1, target_q)
 
 
-def choose_action_from_state(state, model, epoch)
-    ''' Take an action according to a policy and a Q-Learning model.
-    The resulting trasition (s, a, r, s') is stores in a Replay Memory.'''
+def choose_action_from_state(state, model, epoch):
+    """ Makes an action according to eps-greedy policy, observes the result
+    (next state, reward) and learns from the transition"""
 
     def exploration_rate(epoch):
         """# Define exploration rate change over time"""
@@ -153,16 +172,56 @@ def choose_action_from_state(state, model, epoch)
 
     # With probability eps make a random action.
     eps = exploration_rate(epoch)
-    
-    if random() <= eps:
+
+    if random() <= 0.0:
         a = randint(0, len(actions) - 1)
     else:
         # Choose the best action according to the network.
         a = get_best_action(model, state)
 
     return a
-      
-            
+
+
+def generate_memory(epoch, memory, q_network):
+
+    print('gen memory')
+    
+    # create vizdoom game
+    game = initialize_vizdoom()
+    game.new_episode()
+    #episodes_starte.put(
+
+    episode_finished = False
+
+    while not episode_finished:
+        
+        # generate transition and store in memory
+        s_old = preprocess(game.get_state().screen_buffer)
+        
+        a = choose_action_from_state(s_old, q_network, epoch)
+        print(a)
+        #a = randint(0, len(actions) - 1)
+        
+        print('action - %s.: %s' % (current_process().name, memory.get_size()))
+        r = game.make_action(actions[a], frame_repeat)
+
+        print('reward %f - %s.: %s' % (r, current_process().name, memory.get_size()))
+        episode_finished = game.is_episode_finished()
+        s_new = preprocess(game.get_state().screen_buffer) if not episode_finished else None
+
+        print('%s.: %s' % (current_process().name, memory.get_size()))
+        memory.add_transition(s_old, a, s_new, r, episode_finished)
+        print('.%s: %s' % (current_process().name, memory.get_size()))
+        
+    game.close()
+    print('end %s' % (current_process().name))
+    
+    #score = game.get_total_reward()
+    #train_scores.put(score) # train_scores is a Queue
+    
+    #learn_from_memory(model)
+
+
 def initialize_vizdoom():
     print("[1.] Initializing ViZDoom...")
     game = vizdoom.DoomGame()
@@ -176,58 +235,32 @@ def initialize_vizdoom():
     return game
 
 
-def generate_memory(epoch, memory, q_network):
-
-    # create vizdoom game
-    game = initialize_vizdoom()
-    game.new_episode()
-    #episodes_starte.put(
-    
-    while True:
-    
-        # generate transition and store in memory
-        s_old = preprocess(game.get_state().screen_buffer)
-        
-        a = choose_action_from_state(s_old, q_network, epoch)
-
-        r = game.make_action(actions[a], frame_repeat)
-
-        if game.is_episode_finished():
-            s_new = None
-            score = game.get_total_reward()
-            train_scores.put(score) # train_scores is a Queue
-            
-            if train_scores.qsize() < num_episodes:
-                game.new_episode()
-                #episodes_started.put(
-        else:
-            s_new = preprocess(game.get_state().screen_buffer)
-
-        memory.add_transition(s_old, a, s_new, r, isterminal)
-
-
-
 if __name__ == '__main__':
-
+    
     # shared replay memory
     BaseManager.register('ReplayMemory', ReplayMemory)
-    manager = ReplayMemory()
+    manager = BaseManager()
     manager.start()    
     memory = manager.ReplayMemory(capacity=replay_memory_size)
 
-    # create number of possible actions
+    # create vizdoom game
     game = initialize_vizdoom()
     num_actions = game.get_available_buttons_size()
+    game.new_episode()
+    state = preprocess(game.get_state().screen_buffer)
     game.close()
     actions = [list(a) for a in it.product([0, 1], repeat=num_actions)]
 
     # TODO: q network tbm tem q ser atualizada
     q_network = create_network(len(actions))
+    print('network')
+    # initialize network
+    q_network.predict(state.reshape([1, resolution[0], resolution[1], 1]))
     #target_network = create_network(len(actions))
     
-    
-    for epoch in num_epochs:    
-        print("\nEpoch %d\n-------" % (epoch + 1))        
+
+    for epoch in range(num_epochs):    
+        print("\nEpoch %d\n-------" % (epoch + 1))
 
         print("Training...")
         train_scores = []
@@ -236,36 +269,13 @@ if __name__ == '__main__':
         episode = num_episodes
     
         # processes that generate memory
-        for i in range(num_processes):
-            episode = episode - 1
-            p = Process(target=play_episode, args=(epoch, memory, q_network))
-            processes.append(p)
-            p.start()
+        #for i in range(num_processes):
+        #p = Process(target=generate_memory, args=(epoch, memory, q_network))
+        #processes.append(p)
+        #p.start()
+        
+        generate_memory(epoch, memory, q_network)
             
-        # process that updates the networks
-        p = Process(target=learn_from_memory, args(q_network, memory))
-        processes.append(p)
-        p.start()
-            
-        for p in processes:
-            p.join()
+        #for p in processes:
+        #    p.join()
     
-        
-        print("\nTesting...")
-        test_scores = []
-        
-        for test_episode in trange(test_episodes_per_epoch, leave=False):
-            game.set_seed(test_maps.TEST_MAPS[test_episode])
-            game.new_episode()
-            
-            while not game.is_episode_finished():
-                state = preprocess(game.get_state().screen_buffer)
-                best_action_index = get_best_action(q_network, state)
-                game.make_action(actions[best_action_index], frame_repeat)
-                
-            r = game.get_total_reward()
-            test_scores.append(r)
-                    
-        print("Results: mean: %.1f±%.1f," % (test_scores.mean(), test_scores.std()), \
-              "min: %.1f" % test_scores.min(), "max: %.1f" % test_scores.max())
-
